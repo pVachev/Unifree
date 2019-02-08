4254a9dfadbffcfd5b4d16971812258aae466909
kjkj <asdjsdjngfdsikjnhfdsijngfds.bg>
2019-02-08 16:46:00 +0200

    commit 8.2.19

---
+++ args_kwargs_ex.py
@@ -0,0 +1,7 @@
+def test_args(f_arg, *arganmesv):
+    print("first normal arg: " + f_arg)
+    for argnames in arganmesv:
+        print("another argnames through *argnamesv":  + argnames )
+
+
+test_args( )
