40f857e1135c673de35de392ed00967bf1469036
kjkj <asdjsdjngfdsikjnhfdsijngfds.bg>
2019-02-08 16:34:35 +0200

    commit 2.8.19

---
+++ dictionary_exercise.py
@@ -0,0 +1,10 @@
+student = {"student": 'Preslav', "school": 'AUBG', "grades":[100, 90, 60]}
+
+
+def my_func(student)
+    average = sum(student{"grades"])/len(student{"grades"])
+    return average 
+
+print(my_func(student))
+
+
