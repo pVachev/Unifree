a8b69eb783d76f680d1e4908b9a0855ba888b786
kjkj <asdjsdjngfdsikjnhfdsijngfds.bg>
2019-02-08 17:11:17 +0200

    commit 2.8.19

---
+++ module_exercise.py
@@ -0,0 +1,6 @@
+from module_folder import my_func 
+
+
+print(add_function)
+print(subtract_function)
+
---
+++ module_folder.py/my_func.py
@@ -0,0 +1,6 @@
+def add_function(2,3):
+    return 2+3 
+
+
+def subtract_function(2,3):
+    return 2-3 
